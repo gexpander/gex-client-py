c6c847136c231b50edeb9598ece72b16242dfd24
Ondřej Hruška <ondra@ondrovo.com>
2017-12-27 18:17:52 +0100

    added simple bulk access to Unit

--- gex/Unit.py
+++ gex/Unit.py
@@ -14,3 +14,18 @@ class Unit:
     def query(self, cmd, pld=None, id=None):
         """ Query the unit. Returns TF_Msg """
         self.client.query(cs=self.callsign, cmd=cmd, pld=pld, id=None)
+
+    def bulk_read(self, cmd, id=None, pld=None, chunk=1024):
+        """
+        Perform a bulk read.
+        cmd, id and pld are used to initiate the read.
+        """
+        self.client.bulk_read(cs=self.callsign, cmd=cmd, id=id, pld=pld, chunk=chunk)
+
+    def bulk_write(self, cmd, bulk, id=None, pld=None):
+        """
+        Perform a bulk write.
+        cmd, id and pld are used to initiate the read.
+        bulk is the data to write.
+        """
+        self.client.bulk_write(cs=self.callsign, cmd=cmd, id=id, pld=pld, bulk=bulk)
